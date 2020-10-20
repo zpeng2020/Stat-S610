4954c5f0123ad4c85932eb57008ce9c8a2de3274
Zhongwei Peng <zpeng@indiana.edu>
2020-10-20 19:57:20 -0400

    third commit to fill out 1 files with code

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,1 +1,8 @@
- 
+## Stat-S610 Homework 5
+## testing how long the llr function takes to run and prints it out
+
+source("llr_functions.R")
+library(microbenchmark)
+
+time = microbenchmark(llr, times = 1000)
+print(time) 
